415c793b865ce7bbf190a0d186b7018d069adcfa
muuhab <mohab.barca@gmail.com>
2022-03-28 13:15:50 +0200

    Handle Loading

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Header from "./components/header/Header";
-import {lat,long} from './utils/index'
+import { lat, long } from "./utils/index";
 function App() {
-  const key=process.env.REACT_APP_API_KEY
-  console.log(lat)
-  console.log(long)
-  const [weather,setWeather] = useState({})
-  const [daily,setDaily] = useState({})
-  const fetchWeather= async ()=>{
+  const key = process.env.REACT_APP_API_KEY;
+  const [weather, setWeather] = useState({});
+  const [daily, setDaily] = useState({});
+  const [loading, setLoading] = useState(false);
 
-    const result =await axios.get(`https://api.darksky.net/forecast/${key}/${lat},%${long}`)
-    setWeather(result.data.currently)
-    setDaily(result.data.daily.data[0])
-  }
+  const fetchWeather = async () => {
+    setLoading(true);
+    await axios
+      .get(`https://api.darksky.net/forecast/${key}/${lat},%${long}`)
+      .then((data) => {
+        setWeather(data.data.currently);
+        setDaily(data.data.daily.data[0]);
+        setLoading(false);
+      });
+  };
   useEffect(() => {
-    fetchWeather()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [key,lat,long]);
+    fetchWeather();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [key, lat, long]);
   return (
-    <div>
-      <Header current={weather} daily={daily} />
-    </div>
+    <div>{loading ? null : <Header current={weather} daily={daily} />}</div>
   );
 }
 
